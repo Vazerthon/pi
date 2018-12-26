dbc3c0e6d50f87207d901985ab3b8eeb75808f02
Paul Vallis <vallis1@gmail.com>
2018-12-26 14:54:37 +0000

    Add gpio led blink

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello pi')
+import { Gpio } from 'onoff'
+
+const pin18 = new Gpio(18, 'out')
+
+const blink = () => pin18.writeSync(pin18.readSync() === 0 ? 0 : 1)
+
+const stop = () => {
+  clearInterval(blinkInterval)
+  pin18.writeSync(0)
+  pin18.unexport()
+}
+
+const blinkInterval = setInterval(blink, 250)
+setTimeout(stop, 5000)
